a5365a7c6f1e7b27e14dd8497e2c18b4904c17d6
Lise Guiet <lguiet@2B4.42angouleme.fr>
2025-06-25 12:16:28 +0200

    slicing check_colors

--- parsing/check_params/check_colors.c
+++ parsing/check_params/check_colors.c
@@ -6,31 +6,18 @@
 /*   By: lguiet <lguiet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 12:04:25 by lguiet            #+#    #+#             */
-/*   Updated: 2025/06/24 12:35:59 by lguiet           ###   ########.fr       */
+/*   Updated: 2025/06/25 12:15:54 by lguiet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parsing.h"
 #include "../../cub3d.h"
 
-
-int    is_color(char *color_str, int color_int[3])
+int color_valid_char(char *trimmed)
 {
-    char *trimmed;
-    char    **split_clr;
     int i;
-    int res;
 
-    split_clr = NULL;
-    trimmed = ft_strtrim(color_str, " \t \n");
-    if (!trimmed)
-    {
-        perror("malloc");
-        return (0);
-    }
     i = 0;
-    res = -1;
-    printf("trimmed :%s\n", trimmed);
     while(trimmed[i])
     {
         if (!ft_isdigit(trimmed[i]) && trimmed[i] != ',' && trimmed[i] != '\n')
@@ -41,22 +28,36 @@ int    is_color(char *color_str, int color_int[3])
         }
         i++;
     }
-    split_clr = ft_split(trimmed, ',');
-    if(!split_clr)
+    return (1);
+}
+int ft_split_colors(char ***split_clr, char *trimmed)
+{
+    int i;
+
+    *split_clr = ft_split(trimmed, ',');
+    if(!*split_clr)
     {
         free(trimmed);
         return(0);
     }
     i = 0;
-    while(split_clr && split_clr[i])
+    while((*split_clr)[i])
         i++;
     if (i != 3)
     {
         write(2, "Error\ncolors should be [R,G,B]\n", 32);
-        free_map(split_clr);
+        free_map(*split_clr);
         free(trimmed);
         return (0);
     }
+    return (1);
+}
+int get_colors_int(char **split_clr, char *trimmed, int color_int[3])
+{
+    int i;
+    int res;
+
+    res = -1;
     i = 0;
     while(split_clr && split_clr[i])
     {
@@ -71,6 +72,29 @@ int    is_color(char *color_str, int color_int[3])
         color_int[i] = res;    
         i++;
     }
+    return (1);
+}
+
+int    is_color(char *color_str, int color_int[3])
+{
+    char *trimmed;
+    char    **split_clr;
+    int i;
+
+    split_clr = NULL;
+    trimmed = ft_strtrim(color_str, " \t \n");
+    if (!trimmed)
+    {
+        perror("malloc");
+        return (0);
+    }
+    printf("trimmed :%s\n", trimmed);
+    if(!color_valid_char(trimmed))
+        return (0);
+    if (!ft_split_colors(&split_clr, trimmed))
+        return (0);
+    if (!get_colors_int(split_clr, trimmed, color_int))
+        return (0);
     i = 0;
     while (i < 3)
     {
@@ -79,6 +103,5 @@ int    is_color(char *color_str, int color_int[3])
     }
     free(trimmed);
     free_map(split_clr);
-    return (1);
-    
+    return (1);   
 }
